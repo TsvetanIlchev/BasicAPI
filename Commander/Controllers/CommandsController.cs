f55911c8f1ffa893e5392752c355dc9c7148731f
tsvetan.ilchev <tsvetan.ilchev@yahoo.com>
2021-05-24 11:30:12 +0300

    Small API project with few endpoints.

---
+++ Commander/Controllers/CommandsController.cs
@@ -0,0 +1,92 @@
+﻿using AutoMapper;
+using Commander.Data;
+using Commander.Dtos;
+using Commander.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Commander.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly ICommanderRepo m_Repo;
+        private readonly IMapper m_Mapper;
+
+        public CommandsController(ICommanderRepo repository, IMapper mapper)
+        {
+            m_Repo = repository;
+            m_Mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult <IEnumerable<CommandReadDto>> GetAllCommands()
+        {
+            var commandItems = m_Repo.GetAllCommands();
+
+            return Ok(m_Mapper.Map<IEnumerable<CommandReadDto>>(commandItems));
+        }
+
+        [HttpGet("{id}", Name = "GetCommandById")]
+        public ActionResult <CommandReadDto> GetCommandById(int id)
+        {
+            var commandItem = m_Repo.GetCommandById(id);
+
+            if (commandItem != null)
+            {
+                return Ok(m_Mapper.Map<CommandReadDto>(commandItem));
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult <CommandReadDto> CreateCommand(CommandCreateDto commandCreateDto)
+        {
+            Command commandModel = m_Mapper.Map<Command>(commandCreateDto);
+            m_Repo.CreateCommand(commandModel);
+            m_Repo.SaveChanges();
+
+            CommandReadDto commandReadDto = m_Mapper.Map<CommandReadDto>(commandModel);
+
+            return CreatedAtRoute(nameof(GetCommandById), new { commandReadDto.Id }, commandReadDto);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateCommand(int id, CommandUpdateDto commandUpdateDto)
+        {
+            Command commandModel = m_Repo.GetCommandById(id);
+
+            if (commandModel == null)
+            {
+                return NotFound();
+            }
+
+            // Update command
+            m_Mapper.Map(commandUpdateDto, commandModel);
+
+            m_Repo.UpdateCommand(commandModel);
+
+            m_Repo.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteCommand(int id)
+        {
+            Command commandModel = m_Repo.GetCommandById(id);
+
+            if (commandModel == null)
+            {
+                return NotFound();
+            }
+
+            m_Repo.DeleteCommand(commandModel);
+            m_Repo.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
---
+++ Commander/Data/SqlCommanderRepo.cs
@@ -0,0 +1,56 @@
+﻿using Commander.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Commander.Data
+{
+    public class SqlCommanderRepo : ICommanderRepo
+    {
+        private readonly CommanderContext m_Context;
+
+        public SqlCommanderRepo(CommanderContext context)
+        {
+            m_Context = context;
+        }
+
+        public void CreateCommand(Command cmd)
+        {
+            if (cmd == null)
+            {
+                throw new ArgumentNullException(nameof(cmd));
+            }
+
+            m_Context.Commands.Add(cmd);
+        }
+
+        public void DeleteCommand(Command cmd)
+        {
+            if (cmd == null)
+            {
+                throw new ArgumentNullException(nameof(cmd));
+            }
+
+            m_Context.Commands.Remove(cmd);
+        }
+
+        public IEnumerable<Command> GetAllCommands()
+        {
+            return m_Context.Commands.ToList();
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return m_Context.Commands.FirstOrDefault(c => c.Id == id);
+        }
+
+        public bool SaveChanges()
+        {
+           return m_Context.SaveChanges() >= 0;
+        }
+
+        public void UpdateCommand(Command cmd)
+        {
+        }
+    }
+}
